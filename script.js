78c3e0aa4d22115e674d8816e6650140118802db
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 17:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let ans=0;
+
+	for(let i=0;i<library.length;i++){
+		if(library[i].readingStatus)ans++;
+	}
+	return ans;
 };
 
 // Do not change the code below
